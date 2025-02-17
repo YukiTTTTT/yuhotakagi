17734f5eb918b3ac7850a4809c2903f2890b4930
YukiTTTTT <petlianyuki1121@gmail.com>
2025-02-17 23:02:22 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 window.addEventListener('scroll', () => {
     const footer = document.querySelector('footer');
-    const scrollPosition = window.innerHeight + window.scrollY;
+    const scrollPosition = window.innerHeight + window.pageYOffset;
     const bodyHeight = document.body.offsetHeight;
     const distanceFromBottom = bodyHeight - scrollPosition;
     
     if (distanceFromBottom <= 200) {
-      const opacity = 1 - (distanceFromBottom / 200);
-      footer.style.opacity = opacity;
+        const opacity = 1 - (distanceFromBottom / 200);
+        footer.style.opacity = opacity;
     } else {
-      footer.style.opacity = 0;
+        footer.style.opacity = 0;
     }
-  });
-  
+});
