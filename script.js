5f39bff92b174104b1c33e2cdf35b5044976209c
YukiTTTTT <petlianyuki1121@gmail.com>
2025-02-18 00:53:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('.nav-menu');
+    const closeMenu = document.querySelector('.close-menu');
 
     hamburger.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
+        navMenu.classList.add('active');
+    });
+
+    closeMenu.addEventListener('click', function() {
+        navMenu.classList.remove('active');
+    });
+
+    // メニュー外をクリックしたときにもメニューを閉じる
+    document.addEventListener('click', function(event) {
+        if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+            navMenu.classList.remove('active');
+        }
     });
 });
