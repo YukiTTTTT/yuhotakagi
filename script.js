713fe4bfea8b99d09bf0697e41640e4e305126b7
YukiTTTTT <petlianyuki1121@gmail.com>
2025-02-25 23:54:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeMenu = document.querySelector('.close-menu');
     const currentPage = window.location.pathname.split('/').pop();
     const navLinks = document.querySelectorAll('nav ul li a');
+    const bottomCloseButton = document.querySelector('.bottom-close-button');
 
     navLinks.forEach(link => {
         const linkHref = link.getAttribute('href');
@@ -22,10 +23,24 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.remove('active');
     });
 
+    // 右下のCLOSEボタンにイベントリスナーを追加
+    bottomCloseButton.addEventListener('click', function() {
+        navMenu.classList.remove('active');
+    });
+
     // メニュー外をクリックしたときにもメニューを閉じる
     document.addEventListener('click', function(event) {
         if (!navMenu.contains(event.target) && !hamburgerMenu.contains(event.target)) {
             navMenu.classList.remove('active');
+        }
+    });
+
+    navLinks.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        if (linkHref === currentPage || 
+            (currentPage === '' && linkHref === 'index.html') || 
+            (currentPage === '/' && linkHref === 'index.html')) {
+            link.classList.add('active');
         }
     });
 });
